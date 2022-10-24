a1486af9a9775a3849f76824609105cacbfa2455
kostadin9009 kostadin9009 <kostadin9009@students.softuni.bg>
2022-10-24 11:12:40 +0300

    Movies Controller, Movies  Service added

---
+++ Watchlist/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Watchlist.Contracts;
+
+namespace Watchlist.Controllers
+{
+    [Authorize]
+    public class MoviesController : Controller
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var model = await _movieService.GetAllMoviesAsync();
+
+            return View(model);
+        }
+    }
+}
---
+++ Watchlist/Services/MovieService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Watchlist.Contracts;
+using Watchlist.Data;
+using Watchlist.Models;
+
+namespace Watchlist.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly WatchlistDbContext _dbContext;
+
+        public MovieService(WatchlistDbContext dbContext)
+        {
+           _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<MovieViewModel>> GetAllMoviesAsync()
+        {
+            var entities = await _dbContext.Movies
+                                           .Include(m => m.Genre)
+                                           .ToListAsync();
+
+            return entities.Select(x => new MovieViewModel()
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Director = x.Director,
+                ImageUrl = x.ImageUrl,
+                Rating = x.Rating,
+                Genre = x.Genre?.Name
+            });
+        }
+    }
+}
